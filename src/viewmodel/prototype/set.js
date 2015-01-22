6236b0a03ba67357496d85f80d0357e648d193b7
Tim Wigmore-Welsh <tim@wigmore-welsh.com>
2015-01-22 21:37:37 +0000

    Pass teardown option from the adaptor onto clearCache

--- src/viewmodel/prototype/set.js
+++ src/viewmodel/prototype/set.js
@@ -45,12 +45,12 @@ export default function Viewmodel$set ( keypath, value, options = {} ) {
 	}
 
 	if ( !options.silent ) {
-		this.mark( keypath );
+		this.mark( keypath, { keepExistingWrapper: keepExistingWrapper } );
 	} else {
 		// We're setting a parent of the original target keypath (i.e.
 		// creating a fresh branch) - we need to clear the cache, but
 		// not mark it as a change
-		this.clearCache( keypath.str );
+		this.clearCache( keypath.str, keepExistingWrapper );
 	}
 }
 
